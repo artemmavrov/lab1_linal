e35221e0c721cafc02a41a82cf6939b2f3bd21aa
Toxele <mctoxele@gmail.com>
2025-01-25 14:46:28 +0300

    get_matrix_string_sum updated

--- best_file.cpp
+++ best_file.cpp
@@ -150,7 +150,7 @@ void get_matrix_string_sum(map<int, long double> &string_values, Matrix &matrix,
 {
 		for(int values_index = matrix.rowpointer[rowpointer_index - 1]; values_index < matrix.rowpointer[rowpointer_index]; values_index++)
 		{
-			string_values[matrix.col[values_index]] += matrix.values[values_index];
+			string_values[matrix.col[values_index]] += matrix.values[values_index] * matrix.scalar;
 		}
 }
 
